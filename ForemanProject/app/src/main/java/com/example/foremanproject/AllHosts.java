7879c6eca2e0e973653d8313734266a8927318d1
XJH <xjh125470939@gmail.com>
2017-05-16 16:23:22 +0800

    no message

---
+++ ForemanProject/app/src/main/java/com/example/foremanproject/AllHosts.java
@@ -0,0 +1,16 @@
+package com.example.foremanproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by labuser on 5/16/2017.
+ */
+
+public class AllHosts extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_hosts);
+    }
+}
