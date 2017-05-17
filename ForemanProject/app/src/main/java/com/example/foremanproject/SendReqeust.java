5f2f8a69ef963397c709d1ba7a8f5cf7633c880f
XJH <xjh125470939@gmail.com>
2017-05-17 11:52:30 +0800

    no message

--- ForemanProject/app/src/main/java/com/example/foremanproject/SendReqeust.java
+++ ForemanProject/app/src/main/java/com/example/foremanproject/SendReqeust.java
@@ -4,32 +4,14 @@ package com.example.foremanproject;
  * Created by labuser on 5/17/2017.
  */
 
-public class SendReqeust {
+class SendReqeust {
     private static String domain;
     private static String username;
     private static String password;
 
-    public static void setDomain(String newDomain){
-        domain = newDomain;
-    }
+    static void setDomain(String newDomain) { domain = newDomain; }
 
-    public static void setUsername(String newUsername){
-        username = newUsername;
-    }
+    static void setUsername(String newUsername) { username = newUsername; }
 
-    public static void setPassword(String newPassword){
-        password = newPassword;
-    }
-
-    public static String getDomain(){
-        return domain;
-    }
-
-    public static String getUsername(){
-        return username;
-    }
-
-    public static String getPassword(){
-        return password;
-    }
+    static void setPassword(String newPassword) { password = newPassword; }
 }
