bcf5426b556a948b75a414a92f80c8da6238403a
XJH <xjh125470939@gmail.com>
2017-05-24 12:01:16 +0800

    no message

--- ForemanProject/app/src/main/java/com/example/foremanproject/fragment/AllHosts.java
+++ ForemanProject/app/src/main/java/com/example/foremanproject/fragment/AllHosts.java
@@ -1,26 +1,103 @@
 package com.example.foremanproject.fragment;
 
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.foremanproject.R;
+import com.example.foremanproject.other.UserInfo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AllHosts extends Fragment {
     public static AllHosts newInstance() {
         return new AllHosts();
     }
+    LinearLayout totalList;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.all_hosts, container, false);
-        final Activity activity = getActivity();;
+        View view=inflater.inflate(R.layout.all_hosts, container, false);
+        sendRequest();
         return view;
+    }
+
+    private void sendRequest() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, (UserInfo.getUrl() + "api/hosts"), null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            getHosts(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                String auth = Base64.encodeToString(UserInfo.getUNandPW().getBytes(), Base64.NO_WRAP);
+                headers.put("Authorization", "Basic " + auth);
+                return headers;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(jsObjRequest);
+    }
+
+    private void getHosts(JSONObject response) throws JSONException {
+        JSONArray arr = (JSONArray) response.get("results");
+        totalList = (LinearLayout) getView().findViewById(R.id.totallist);
+        for(int i=0;i<arr.length();i++){
+            JSONObject obj = (JSONObject) arr.get(i);
+
+            LinearLayout linearlayout = new LinearLayout(getActivity());
+            linearlayout.setOrientation(LinearLayout.HORIZONTAL);
+            totalList.addView(linearlayout);
+
+            TextView textView = new TextView(getActivity());
+            textView.setText((String) obj.get("name"));
+            textView.setTextSize(24);
+            textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+            Button button = new Button(getActivity());
+            button.setLayoutParams(new LinearLayout.LayoutParams(200, 160));
+            button.setText("Edit");
+            button.setTag(obj.get("name"));
+            button.setBackground(getResources().getDrawable(R.drawable.button_icon));
+            linearlayout.addView(textView);
+            linearlayout.addView(button);
+
+            totalList.addView(new LinearLayout(getActivity()));
+        }
     }
 }
