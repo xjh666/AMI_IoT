e0b8ddb4e950bbe0bb60dbe2f342c4621b603eda
XJH <xjh125470939@gmail.com>
2017-05-26 16:35:04 +0800

    no message

--- ForemanProject/app/src/main/java/com/example/foremanproject/activity/Parameters.java
+++ ForemanProject/app/src/main/java/com/example/foremanproject/activity/Parameters.java
@@ -41,6 +41,10 @@ public class Parameters extends AppCompatActivity {
     private HashMap<String, ArrayList<JSONObject>> parameters;
 
     @Override
+    public void onBackPressed() {
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parameters);
