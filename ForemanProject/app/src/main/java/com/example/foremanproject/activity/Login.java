347feb5b667434005d2bb082d5f849847682f862
XJH <xjh125470939@gmail.com>
2017-06-19 11:31:15 +0800

    no message

--- ForemanProject/app/src/main/java/com/example/foremanproject/activity/Login.java
+++ ForemanProject/app/src/main/java/com/example/foremanproject/activity/Login.java
@@ -46,7 +46,12 @@ public class Login extends AppCompatActivity {
     EditText passwordEdit;
 
     @Override
-    public void onBackPressed() {}
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
