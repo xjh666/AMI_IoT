1c7a0abd4c12f8f14b2afd366e2c9b594dd6f081
XJH <xjh125470939@gmail.com>
2017-06-21 12:13:55 +0800

    no message

--- ForemanProject/app/src/main/java/com/example/foremanproject/activity/ConfigReportDetail.java
+++ ForemanProject/app/src/main/java/com/example/foremanproject/activity/ConfigReportDetail.java
@@ -2,6 +2,7 @@ package com.example.foremanproject.activity;
 
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
@@ -24,6 +25,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -80,6 +82,15 @@ public class ConfigReportDetail extends AppCompatActivity {
         LinearLayout list = (LinearLayout) findViewById(R.id.list);
         LinearLayout layout;
         TextView text;
+
+        text = new TextView(this);
+        text.setText(" Reported at " + response.getString("reported_at").substring(0,10) + " " + response.getString("reported_at").substring(11,19) + " " +response.getString("reported_at").substring(23));
+        list.addView(text);
+
+        text = new TextView(this);
+        text.setText("");
+        text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int)(Configuration.getHeight()* 0.01)));
+        list.addView(text); // space
 
         layout = new LinearLayout(this);
         text = new TextView(this);
@@ -148,6 +159,67 @@ public class ConfigReportDetail extends AppCompatActivity {
             list.addView(layout);
         }
 
+        text = new TextView(this);
+        text.setText("");
+        text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int)(Configuration.getHeight()* 0.05)));
+        list.addView(text); // space
+
+        text = new TextView(this);
+        text.setText(" Report Time");
+        list.addView(text); // space
+
+        Iterator<?> keys = response.getJSONObject("metrics").getJSONObject("time").keys();
+        while(keys.hasNext()) {
+            String key = (String)keys.next();
+            if(key.equals("total")) continue;
+
+            if(!(response.getJSONObject("metrics").getJSONObject("time").getDouble(key)<0.001)){
+                layout = new LinearLayout(this);
+
+                text = new TextView(this);
+                text.setText(key);
+                text.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                text.setTextSize(16);
+                text.setTextColor(Color.BLACK);
+                    text.setLayoutParams(new LinearLayout.LayoutParams((int)(Configuration.getWidth()* 0.5), LinearLayout.LayoutParams.WRAP_CONTENT));
+                text.setBackgroundResource(R.drawable.cell_shape);
+                layout.addView(text);
+
+                text = new TextView(this);
+                text.setText(" " + (response.getJSONObject("metrics").getJSONObject("time").getString(key).substring(0,6)));
+                text.setTextSize(16);
+                text.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                text.setTextColor(Color.BLACK);
+                text.setLayoutParams(new LinearLayout.LayoutParams((int)(Configuration.getWidth()* 0.505), LinearLayout.LayoutParams.MATCH_PARENT));
+                text.setBackgroundResource(R.drawable.cell_shape);
+                layout.addView(text);
+
+                list.addView(layout);
+            }
+        }
+        layout = new LinearLayout(this);
+
+        text = new TextView(this);
+        text.setText("Total");
+        text.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        text.setTextSize(16);
+        text.setTextColor(Color.BLACK);
+        text.setLayoutParams(new LinearLayout.LayoutParams((int)(Configuration.getWidth()* 0.5), LinearLayout.LayoutParams.WRAP_CONTENT));
+        text.setBackgroundResource(R.drawable.cell_shape);
+        text.setTypeface(null, Typeface.BOLD);
+        layout.addView(text);
+
+        text = new TextView(this);
+        text.setText(" " + (response.getJSONObject("metrics").getJSONObject("time").getString("total").substring(0,6)));
+        text.setTextSize(16);
+        text.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        text.setTextColor(Color.BLACK);
+        text.setLayoutParams(new LinearLayout.LayoutParams((int)(Configuration.getWidth()* 0.505), LinearLayout.LayoutParams.MATCH_PARENT));
+        text.setBackgroundResource(R.drawable.cell_shape);
+        text.setTypeface(null, Typeface.BOLD);
+        layout.addView(text);
+
+        list.addView(layout);
     }
 
     public static void setInfo(Map<String, Object> tag){
